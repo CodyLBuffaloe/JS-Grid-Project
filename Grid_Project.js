7ffc7dc7e779a542c99e7cc736f6ac6d66238804
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-04 17:29:35 -0500

    Created a grid that user populates, next step is to animate

--- Grid_Project.js
+++ Grid_Project.js
@@ -1,9 +1,23 @@
 /**
  * Created by codybuffaloe on 12/21/15.
  */
-$(document).ready(){
-    function createGrid(){
-        
-    }
+var squares = 2;
+$(document).ready(function(){
+    squares= prompt('How many squares would you like? (Creates a grid of x by x squares)');
+    createGrid(squares);
+});
 
-}
+function createGrid(squares){
+    for(var i= 0;i<squares*squares;i++){
+    $('.container').append('<div class="grid"></div>');
+    }
+    var high= parseFloat($('.container').css('height').replace('px'," "));
+    var wide= parseFloat($('.container').css('width').replace('px',' '));
+
+    high=parseFloat((high-2*squares)/squares);
+    wide= parseFloat((wide-2*squares)/squares);
+
+    $(".grid").css("height",high);
+    $(".grid").css("width",wide);
+
+};
